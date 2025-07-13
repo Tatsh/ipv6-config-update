46cf79f67bc848e119ef94574fa3d8ad5e12e58b
Andrew Udvare <audvare@gmail.com>
2025-07-12 20:59:27 -0400

    src/test: add

---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+#include <QtDBus/QtDBus>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using ::testing::_;
+using ::testing::Return;
+
+TEST(GenerateAddressesTests, Blah) {
+    ASSERT_EQ(0, 0);
+}
